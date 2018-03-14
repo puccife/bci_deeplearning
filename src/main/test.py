59982204311ccd3ee94c3c0c7731efea0cec4415
christian-5-28 <christian.sciuto@epfl.ch>
2018-03-14 08:59:05 +0100

    download data

--- src/main/test.py
+++
@@ -1,8 +0,0 @@
-import utils.dlc_bci as bci
-
-train_input , train_target = bci.load(root='../../data_bci', one_khz = False)
-print ( str ( type ( train_input ) ) , train_input.size())
-print ( str ( type ( train_target ) ) , train_target.size())
-test_input , test_target = bci.load ( root = '../../data_bci', train = False, one_khz = False)
-print ( str ( type ( test_input ) ) , test_input.size())
-print ( str ( type ( test_target ) ) , test_target.size())
