f6c572edd6a20bdf4d249ed193d0b6a9a2bf5d0c
christian-5-28 <christian.sciuto@epfl.ch>
2018-03-28 11:45:13 +0200

    initialization weights

--- src/main/train_test.py
+++ src/main/train_test.py
@@ -1,7 +1,10 @@
 from model.cnn.train_cnn import CNNTrainer
+from model.cnn.basic_cnn import CNN
+
+net = CNN()
 
 def run():
-    CNNTrainer().train_cnn()
+    CNNTrainer(cnn=net).train_cnn()
 
 if __name__ == "__main__":
     run()
