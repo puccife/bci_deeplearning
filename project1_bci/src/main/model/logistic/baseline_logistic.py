1eacc6f92ea4b1f04504cb7666ff44e76b84ff14
Christian Sciuto <sciutochristian@gmail.com>
2018-05-03 15:25:50 +0200

    scp algorithm implemented. logistic accuracy improved to 90%

--- project1_bci/src/main/model/logistic/baseline_logistic.py
+++ project1_bci/src/main/model/logistic/baseline_logistic.py
@@ -5,6 +5,8 @@ import project1_bci.src.main.utils.dlc_bci as bci
 import torch.utils.data as dt
 import numpy as np
 from EegDataset import EegDataset
+from project1_bci.src.main.preprocessing.preprocessing import Preprocess
+
 
 # Hyper Parameters
 
@@ -12,12 +14,35 @@ num_classes = 2
 num_epochs = 300
 batch_size = 2
 learning_rate = 0.001
-
+'''
 train_dataset = EegDataset(data_path='/media/sf_semester_project/bci_deeplearning/project1_bci/data_bci',
                            model='log', training=True)
 test_dataset = EegDataset(data_path='/media/sf_semester_project/bci_deeplearning/project1_bci/data_bci', model='log',
                           training=False)
+'''
 
+data_path = '../../data_bci'
+train_inputs, train_targets = bci.load(root=data_path, one_khz=False)
+test_inputs, test_targets = bci.load(root=data_path, train=False, one_khz=False)
+
+# normalize inputs
+pp = Preprocess()
+train_inputs, test_inputs = pp.transform(train_inputs, test_inputs, permutate=False, model=None)
+
+# testing the behavior with variance application for flatten the samples
+'''
+result = np.var(train_inputs.numpy(), axis=2)
+train_inputs = torch.from_numpy(result)
+ts_result = np.var(test_inputs.numpy(), axis=2)
+test_inputs = torch.from_numpy(ts_result)
+'''
+# flatten each sample in order to be fitted to the Logistic classifier
+train_inputs = train_inputs.view((train_inputs.size()[0], -1))
+test_inputs = test_inputs.view((test_inputs.size()[0], -1))
+
+
+train_dataset = dt.TensorDataset(train_inputs, train_targets)
+test_dataset = dt.TensorDataset(test_inputs, test_targets)
 
 '''
 # flatten the values of our inputs for the SVM
@@ -51,9 +76,9 @@ class LogisticRegression(nn.Module):
         out = self.linear(x)
         return out
 
-shape = train_dataset.train_inputs.shape[1]
+shape = train_inputs.shape[1]
 
-model = LogisticRegression(train_dataset.train_inputs.shape[1], num_classes)
+model = LogisticRegression(train_inputs.shape[1], num_classes)
 
 # Loss and Optimizer
 # Softmax is internally computed.
