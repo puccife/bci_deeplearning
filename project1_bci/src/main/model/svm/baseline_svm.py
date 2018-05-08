3c51c0d1efe8524d0cfb4ec6b7fd46dceabb6612
Christian Sciuto <sciutochristian@gmail.com>
2018-05-08 19:33:27 +0200

    scp on svm

--- project1_bci/src/main/model/svm/baseline_svm.py
+++ project1_bci/src/main/model/svm/baseline_svm.py
@@ -1,3 +1,5 @@
+from scipy import signal
+
 import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
@@ -6,6 +8,48 @@ from torch.autograd import Variable
 import project1_bci.src.main.utils.dlc_bci as bci
 import torch.utils.data as dt
 import numpy as np
+from eegtools import spatfilt
+from sklearn import base
+from project1_bci.src.main.preprocessing.preprocessing import Preprocess
+
+
+# Create sklearn-compatible feature extraction and classification pipeline:
+class CSP(base.BaseEstimator, base.TransformerMixin):
+    def fit(self, X, y):
+        class_covs = []
+
+        # calculate per-class covariance
+        for ci in np.unique(y):
+            class_mask = y == ci
+            num_ones = class_mask.sum()
+            x_filtered = X[class_mask]
+            # to_cov = np.hstack(X[class_mask])
+            to_cov = np.concatenate(x_filtered, axis=1)
+            class_covs.append(np.cov(to_cov))
+        assert len(class_covs) == 2
+
+        # calculate CSP spatial filters, the third argument is the number of filters to extract
+        self.W = spatfilt.csp(class_covs[0], class_covs[1], 8)
+        print("csp concluded, spatial filter computed!")
+        return self
+
+    def transform(self, X):
+        # Note that the projection on the spatial filter expects zero-mean data.
+        print("projection on the spatial filter started!")
+        projection = np.asarray([np.dot(self.W, trial) for trial in X])
+        return projection
+
+
+class ChanVar(base.BaseEstimator, base.TransformerMixin):
+    def fit(self, X, y): return self
+
+    def transform(self, X):
+        print("variance on channels started!")
+        result = np.var(X, axis=2)  # X.shape = (trials, channels, time)
+        return result
+
+#################################################
+
 
 # Hyper Parameters
 
@@ -13,32 +57,78 @@ num_classes = 2
 num_epochs = 300
 batch_size = 2
 learning_rate = 0.0001
+data_path = '../../data_bci'
+train_inputs, train_targets = bci.load(root=data_path, one_khz=False)
+test_inputs, test_targets = bci.load(root=data_path, train=False, one_khz=False)
 
-train_input, train_target = bci.load(root='/media/sf_semester_project/bci_deeplearning/project1_bci/data_bci',
-                                     one_khz=True)
-test_input, test_target = bci.load(root='/media/sf_semester_project/bci_deeplearning/project1_bci/data_bci',
-                                   train=False, one_khz=True)
+# normalize inputs
+pp = Preprocess()
+train_inputs, test_inputs = pp.transform(train_inputs, test_inputs, permutate=False, model=None)
 
-#normalize input
-mean = torch.mean(train_input)
-std = torch.std(train_input)
-train_input = (train_input - mean) / std
+train_targets = torch.Tensor([elem if elem == 1 else -1 for elem in train_targets])
+test_targets = torch.Tensor([elem if elem == 1 else -1 for elem in test_targets])
 
-mean = torch.mean(test_input)
-std = torch.std(test_input)
-test_input = (test_input - mean) / std
+train_inputs = train_inputs.numpy()
+train_targets = train_targets.numpy()
+test_inputs = test_inputs.numpy()
+test_targets = test_targets.numpy()
+
+count = 0
+id_elem_removed = []
+for index, target in enumerate(train_targets):
+    if target == 0:
+        count += 1
+        if count > 2:
+            break
+        id_elem_removed.append(index)
+train_inputs = np.delete(train_inputs, id_elem_removed, axis=0)
+train_targets = np.delete(train_targets, id_elem_removed, axis=0)
+
+count = 0
+id_test_removed = []
+for index, target in enumerate(test_targets):
+    if target == 0:
+        count += 1
+        if count > 2:
+            break
+        id_test_removed.append(index)
+
+test_inputs = np.delete(test_inputs, id_test_removed, axis=0)
+test_targets = np.delete(test_targets, id_test_removed, axis=0)
+
+print(train_inputs.shape)
+print(train_targets.shape)
+print(test_inputs.shape)
+print(test_targets.shape)
+
+# create band-pass filter for the  8--30 Hz where the power change is expected
+sample_rate = 100
+(b, a) = signal.butter(6, np.array([8, 30]) / (sample_rate / 2), btype='bandpass')
+
+# band-pass filter the EEG
+train_inputs = signal.lfilter(b, a, train_inputs, 2)
+test_inputs = signal.lfilter(b, a, test_inputs, 2)
+
+###############################################
 
 
-# flatten the values of our inputs for the SVM
-train_input = train_input.view((train_input.size()[0], -1))
-test_input = test_input.view((test_input.size()[0], -1))
 
-train_target = torch.FloatTensor([elem if elem == 1 else -1 for elem in train_target])
-test_target = torch.FloatTensor([elem if elem == 1 else -1 for elem in test_target])
+# projecting on spacial filters
+csp = CSP()
+csp.fit(train_inputs, train_targets)
+train_inputs = csp.transform(train_inputs)
+train_inputs = ChanVar().transform(train_inputs)
+train_inputs = torch.from_numpy(train_inputs)
+train_targets = torch.from_numpy(train_targets)
 
+csp_test = CSP().fit(test_inputs, test_targets)
+test_inputs = csp_test.transform(test_inputs)
+test_inputs = ChanVar().transform(test_inputs)
+test_inputs = torch.from_numpy(test_inputs)
+test_targets = torch.from_numpy(test_targets)
 
-train_dataset = dt.TensorDataset(train_input, train_target)
-test_dataset = dt.TensorDataset(test_input, test_target)
+train_dataset = dt.TensorDataset(train_inputs, train_targets)
+test_dataset = dt.TensorDataset(test_inputs, test_targets)
 
 
 train_loader = dt.DataLoader(dataset=train_dataset,
@@ -61,7 +151,7 @@ class SVM(nn.Module):
         return out
 
 
-model = SVM(train_input.shape[1], num_classes)
+model = SVM(train_inputs.shape[1], num_classes)
 
 # Loss and Optimizer
 # Softmax is internally computed.
@@ -74,7 +164,7 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 for epoch in range(num_epochs):
     av_losses = []
     for i, (images, labels) in enumerate(train_loader):
-        images = Variable(images, requires_grad=True)
+        images = Variable(images.float(), requires_grad=True)
         labels = Variable(labels)
 
         # Forward + Backward + Optimize
@@ -94,7 +184,7 @@ total = 0
 pred = []
 gt = []
 for images, labels in test_loader:
-    images = Variable(images)
+    images = Variable(images.float())
     outputs = model(images)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
