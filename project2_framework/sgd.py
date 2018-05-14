c60e583b8e4be26722fb9c815f450c7706c695ec
okm02 <omarkmehio@gmail>
2018-05-14 12:15:03 +0200

    added normalization of gradient

--- project2_framework/sgd.py
+++ project2_framework/sgd.py
@@ -2,14 +2,14 @@ import torch
 
 class SGD:
 
-	def compute_stoch_gradient(self,backward_pass,forward_pass):
+	def compute_stoch_gradient(self,backward_pass,forward_pass,mini_batch_size):
 			
 		gradient = []
 			
 		for i in range(0, len(backward_pass)):
 			 # gradient for sgd w.r.t weight = dot(x(l),delta(l+1).T)	
 			
-			weight_loss = torch.mm(backward_pass[i], torch.transpose(forward_pass[i][1], 0,1)) 
+			weight_loss = torch.mm(backward_pass[i], torch.transpose(forward_pass[i][1], 0,1))  * 1/mini_batch_size
 			bias_loss = backward_pass[i]  # bias is just equal to delta
 			gradient.append((torch.transpose(weight_loss,0,1), bias_loss )) 
 			# we transpose final result since we are using column wise ops
