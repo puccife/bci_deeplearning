eafbe9af9ecaf5b307ab40a8e17889fa71aa7935
puccife <federico.pucci@epfl.ch>
2018-05-16 18:37:04 +0200

    formatted trainer

--- project2_framework/trainer.py
+++ project2_framework/trainer.py
@@ -1,38 +1,37 @@
 from nn.optimizers.SGD import sgd
 
+# from tensorboardX import SummaryWriter
+
 def train(model, epochs, train_loader, test_loader, loss, lr=0.01):
 
+    # writer = SummaryWriter()
     for epoch in range(epochs):
-        epoch_train_losses = []
-        for x_batch, y_batch in train_loader.get_loader():
+        for k, (x_batch, y_batch) in enumerate(train_loader.get_loader()):
             model, batch_loss = sgd(model=model, x_batch=x_batch, y_batch=y_batch, loss=loss, lr=lr)
-            epoch_train_losses.append(batch_loss)
-            # print('Epoch [%d/%d], batch Loss: %.9f' % (epoch + 1, epochs, batch_loss))
 
-        epoch_train_loss = sum(epoch_train_losses) / len(epoch_train_losses)
+        t_loss, t_accuracy = validate('train', train_loader, model, epoch, loss)#, writer)
+        v_loss, v_accuracy = validate('validation', test_loader, model, epoch, loss)#, writer)
 
-        # validation
-        epoch_val_losses = []
-        total = 0
-        correct = 0
-        for x_batch, y_batch in test_loader.get_loader():
-            for x, y in zip(x_batch, y_batch):
-                predicted = model.forward(x)
-                total+=1
-                if predicted.max(0)[1][0] == y.max(0)[1][0]:
-                    correct+=1
-                loss_value = loss.compute(predicted, y)
-                epoch_val_losses.append(loss_value)
-
-        epoch_val_loss = sum(epoch_val_losses) / len(epoch_val_losses)
-
-        print("Accuracy: ", correct/total)
-        print('Epoch [%d/%d], train Loss: %.6f, val loss: %.6f'% (epoch + 1, epochs,
-                 epoch_train_loss, epoch_val_loss))
+        print("Epoch [",epoch,"/",epochs,"], train acc: ", t_accuracy, ", val acc: ", v_accuracy)
+        print('Epoch [ %d / %d ], train loss: %.6f, val loss: %.6f'% (epoch + 1, epochs,
+                 t_loss, v_loss))
 
 
-
-
-
-
-
+def validate(label, loader, model, epoch, loss):#, writer):
+    epoch_val_losses = []
+    total = 0
+    correct = 0
+    for j, (x_batch, y_batch) in enumerate(loader.get_loader()):
+        batch_loss = 0
+        for x, y in zip(x_batch, y_batch):
+            predicted = model.forward(x)
+            total += 1
+            if predicted.max(0)[1][0] == y.max(0)[1][0]:
+                correct += 1
+            loss_value = loss.compute(predicted, y)
+            batch_loss += loss_value
+            epoch_val_losses.append(loss_value)
+        # writer.add_scalars('loss', {label: batch_loss}, epoch * 1000 + 10 * j)
+        # writer.add_scalars('accuracy', {label: correct/total}, epoch * 1000 + 10 * j)
+    epoch_val_loss = sum(epoch_val_losses) / len(epoch_val_losses)
+    return epoch_val_loss, correct/total
